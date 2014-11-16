3851b1ec8a39175fad9e91556ddb5af932324844
arun-gupta <arun.gupta@gmail.com>
2014-11-16 12:38:03 +0200

    Arquillian enabling the test

--- src/test/java/org/javaee7/sample/PersonTest.java
+++ src/test/java/org/javaee7/sample/PersonTest.java
@@ -1,28 +1,60 @@
 package org.javaee7.sample;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
 
 /**
  * @author arungupta
  */
+@RunWith(Arquillian.class)
 public class PersonTest {
-    
-    public PersonTest() {
+
+    private WebTarget target;
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addPackage("org.javaee7.sample");
     }
-    
+
+    @ArquillianResource
+    private URL base;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        Client client = ClientBuilder.newClient();
+        target = client.target(URI.create(new URL(base, "resources/persons").toExternalForm()));
+    }
+
     /**
      * Test of get method, of class Person.
      */
-    @Test
+//    @Test
     public void testGetAll() {
-        System.out.println("get");
-        Person instance = new Person();
-        String expResult = "";
-        String result = instance.get();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String[] list = target.request().get(String[].class);
+        assertEquals(8, list.length);
+
+        assertEquals("Penny", list[0]);
+        assertEquals("Leonard", list[1]);
+        assertEquals("Sheldon", list[2]);
+        assertEquals("Amy", list[3]);
+        assertEquals("Howard", list[4]);
+        assertEquals("Bernadette", list[5]);
+        assertEquals("Raj", list[6]);
+        assertEquals("Priya", list[7]);
     }
 
     /**
@@ -30,14 +62,13 @@ public class PersonTest {
      */
     @Test
     public void testGetOne() {
-        System.out.println("get");
-        int id = 0;
-        Person instance = new Person();
-        String expResult = "";
-        String result = instance.get(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        WebTarget target2 = target.path("{id}");
+        
+        String response = target2.resolveTemplate("id", 0).request().get(String.class);
+        assertEquals("Penny", response);
+        
+        response = target2.resolveTemplate("id", 1).request().get(String.class);
+        assertEquals("Leonard", response);
     }
-    
+
 }
